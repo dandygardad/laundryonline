32ed74e7f0d86ba04223714500cbdf085bd4130c
dandygardad <dandygardad@gmail.com>
2020-05-31 01:02:31 +0800

    Add prevent empty signup form and duplicate e-mail submitted to database

--- laundry.sql
+++ laundry.sql
@@ -2,8 +2,8 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) DEFAULT NULL,
   `email` varchar(128) DEFAULT NULL,
-  `password` VARCHAR(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   `nomor_telepon` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
--- signup.php
+++ signup.php
@@ -1,36 +1,65 @@
 <!DOCTYPE html>
 <?php
+session_start();
 require_once "database.php";
 
 if (isset($_POST['submit'])){
-    if(empty($_POST['name']) && empty($_POST['email']) && empty($_POST['password']) && empty($_POST['nomorTelepon'])) {
-        exit('Field kosong');
+
+    //Mencegah ada e-mail yang sama dalam database
+    $email = $_POST['email'];
+    $result = $pdo -> prepare("SELECT email FROM users WHERE email = :email");
+    $result -> bindParam(':email', $email);
+    $result -> execute();
+
+    //Mencegah data kosong masuk database
+    if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['nomorTelepon'])) {
+        echo '<div class="box"><div class="square">';
+        echo("Field tidak boleh kosong!");
+        echo '</div></div>';
     }
-    $sql = "INSERT INTO users (name, email, password, nomor_telepon) 
-              VALUES (:name, :email, :password, :nomor_telepon)";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute(array(
-        ':name' => $_POST['name'],
-        ':email' => $_POST['email'],
-        ':password' => $_POST['password'],
-        ':nomor_telepon' => $_POST['nomorTelepon']));
-}
+
+    //Pesan yang muncul jika sudah ada e-mail dalam database
+    else if($result->rowCount() > 0){
+        echo '<div class="box"><div class="square">';
+        echo("E-mail sudah ada, silahkan gunakan e-mail yang berbeda!");
+        echo '</div></div>';
+    }
+
+    //Input Data
+    else{
+        echo '<div class="box"><div class="square">';
+        echo $_SESSION['message'];
+        echo '</div></div>';
+        unset($_SESSION['message']);
+        $sql = "INSERT INTO users (name, email, password, nomor_telepon) 
+        VALUES (:name, :email, :password, :nomor_telepon)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(
+          ':name' => $_POST['name'],
+          ':email' => $_POST['email'],
+          ':password' => $_POST['password'],
+          ':nomor_telepon' => $_POST['nomorTelepon']));
+        $_SESSION['message'] = 'Data sudah masuk, silahkan kembali <a href="/login.php">Login</a> ';
+    }
+} 
 ?>
 
 <head>
     <title>Signup - Laundry OnLine</title>
     <link rel="stylesheet" type="text/css" href="signup.css">
+    <link rel="stylesheet" type="text/css" href="signup_2.css">
 </head>
 
 <body>
     <div class="login-page">
         <div class="form">
-            <h1>Signup</h1>        
+            <h1>Signup</h1>   
+            <br>     
             <form method="POST">
                 <p>Nama :</p>
                 <input type="text" name="name">
                 <p>E-Mail :</p>
-                <input type="text" name="email">
+                <input type="email" name="email">
                 <p>Password :</p>
                 <input type="password" name="password">
                 <p>Nomor Telepon :</p>
