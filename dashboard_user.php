c148881b7b1c817aab38e4f9e606cf04c45e6fad
nurhasanabunawas <nurhasanaabunawas>
2020-06-02 22:37:04 +0800

    add dashboard user

---
+++ dashboard_user.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<?php
+
+require_once "database.php";
+
+?>
+<html lang="en">
+<head>
+        <link rel="stylesheet" type="text/css" href="dashboard_user.css">
+        <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profiles</title>
+</head>
+<body>
+<div >
+        <ul class="onclick">
+            
+            <li><a href="login">Home</a></li>
+            <li><a href="produk">Product</a></li> 
+            <li><a href="layanan">Services</a></li>
+          </ul>    
+</div>
+
+
+<div class="container2">  <h1>Profiles</h1>   
+        <div>   
+        <div class="title">Email</div>
+                <input type="text" name="projectData[dateInputOther]" placeholder="email" readonly>
+        </div>
+
+        <div>
+         <div class="title">Address</div>   
+                <input type="text" name="projectData[dateInputOther]" placeholder="address" readonly>
+        </div>  
+
+        <div>   
+        <div class="title">Contact</div>
+                <input type="text" name="projectData[dateInputOther]" placeholder="contact" readonly>
+        </div>
+</div>
+
+<div class="container3">
+            <h1>FAQ</h1>
+            <h2>Frequently Asked Questions</h2>
+            <h3>What is online laundry?</h3>
+            <p> online laundry help you with your laundry. powered by web application, 
+                we partnered with professional laundry and offer pick up and delivery service right to your house, office or other places.</p>
+            <h3> How to order online laundry?</h3>
+            <p>You can order via our website application.</p>
+            <h3> Why use online laundry ?</h3>
+            <p>Free pick up and delivery, track your order, fast services, best quality with competitve price and service guarantee.</p>
+    </div>
+
+<div class="container4">
+        
+                 <H4>address</H4>
+                Jl. abc No. 40B, Sulawesi Selatan 12560 
+
+                <H4>contact us!</H4>
+                (021) 227 1XX XX
+          
+                 <h4>Follow US</h4>
+                
+         
+
+            <li> <!-- Ulangi lagi tiga kali sehingga sekarang kita punya 6 list -->
+            <li> <!-- Ulangi lagi tiga kali sehingga sekarang kita punya 6 list -->
+            <li> <!-- Ulangi lagi tiga kali sehingga sekarang kita punya 6 list -->
+        </ul>
+
+</body>
+</html>
